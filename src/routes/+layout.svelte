73d24600ab3cae16a2682039acf3bf978889a694
Hojun Lee <hppmm@naver.com>
2024-12-27 19:52:26 +0900

    Home, Login routes

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,8 +3,9 @@
 	let { children } = $props();
 </script>
 
-<nav>
-	<a href="/">Home</a>
+<nav class="flex justify-end p-4 gap-4 fixed top-0 left-0 right-0 items-center text-xl">
+	<a href="/"><ion-icon name="home-outline">HOME</ion-icon></a>
+	<a href="/login"><ion-icon name="log-in-outline">LOGIN</ion-icon></a>
 </nav>
 
 {@render children()}
