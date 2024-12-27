d4625d02211d22a5ba2cc380ff8b722c88fd35bf
Hojun Lee <hppmm@naver.com>
2024-12-28 06:54:52 +0900

    로그인 페이지 작업

---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,17 @@
+<div class="rounded-lg p-4 flex flex-col items-center justify-center w-dvw h-dvh align-middle">
+    <div class="flex flex-col gap-4 bg-white p-4 rounded-lg shadow-lg w-96">
+        <div class="flex justify-between items-center text-2xl text-center">
+            <h1 class="font-bold text-indigo-600 select-none">LOGIN</h1>
+            <a href="/" class="flex flex-col justify-center"><ion-icon name="arrow-back-outline">BACK</ion-icon></a>
+        </div>
+        <div>
+            <input type="text" class="w-full p-2 border border-gray-300 rounded-lg mt-2" placeholder="ID" />
+        </div>
+        <div>
+            <input type="text" class="w-full p-2 border border-gray-300 rounded-lg mt-2" placeholder="PASSWORD" />
+        </div>
+        <div>
+            <button class="w-full p-2 bg-indigo-600 text-white rounded-lg mt-2">LOGIN</button>
+        </div>
+    </div>
+</div>
