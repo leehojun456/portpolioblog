0b00f9506b83bce00464bde994d4976622f364a2
Hojun Lee <hppmm@naver.com>
2024-12-27 14:43:17 +0900

    랜딩 페이지 작업 중

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="rounded-lg p-4 flex flex-col items-center justify-center w-dvw h-dvh align-middle">
+    <h1 class="font-bold text-indigo-600 select-none text-9xl">CALB</h1>
+    <div class="text-gray-500 text-2xl select-none">
+        Developer
+    </div>
+</div>
