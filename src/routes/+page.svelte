a0f244ba06eb01349a474e2f75cf0d367d923d31
Hojun Lee <hppmm@naver.com>
2024-12-28 16:44:29 +0900

    랜딩페이지 네비게이션 추가

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,3 +4,23 @@
         Developer
     </div>
 </div>
+
+<a href="/" class="flex fixed bottom-12 justify-center w-full">
+    <div class="text-gray-500 text-2xl select-none tracking-[1em]">
+        <p class="border-t-4 p-4 border-indigo-200 hover:border-indigo-600" style="text-indent: 1em;">
+            BLOG
+        </p>
+    </div>
+</a>
+
+<a href="/" class="flex flex-col fixed top-0 bottom-0 right-24 justify-center h-full"> 
+    <div class="text-gray-500 text-2xl select-none tracking-[1em]">
+        <p class="border-l-4 p-4 border-indigo-200 hover:border-indigo-600" style="writing-mode: vertical-rl; text-orientation: upright; text-indent: 1em;">ABOUT</p>
+    </div>
+</a>
+
+<a href="/" class="flex flex-col fixed top-0 bottom-0 left-24 justify-center h-full"> 
+    <div class="text-gray-500 text-2xl select-none tracking-[1em]">
+        <p class="border-r-4 p-4 border-indigo-200 hover:border-indigo-600" style="writing-mode: vertical-lr; text-orientation: upright; text-indent: 1em;">FOLIO</p>
+    </div>
+</a>
