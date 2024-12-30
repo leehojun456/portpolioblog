93fb314f506ed2b54bd03158ca5ab6359a86ad0f
Hojun Lee <hppmm@naver.com>
2024-12-30 19:39:40 +0900

    나만 사용하려했다가 향후 서비스 계획으로 변경

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,25 +1,21 @@
+<script>
+	import '../app.css';
+	let { children } = $props();
+</script>
 
-    <h1 class="font-bold text-indigo-600 select-none text-9xl">CALB</h1>
-    <div class="text-gray-500 text-2xl select-none">
-        Developer
+<div class="flex flex-col justify-center h-dvh items-center w-full">
+    <div class="flex flex-col w-2/3 gap-96 ">
+        <div class=" font-bold text-9xl text-indigo-600 flex flex-col select-none">
+            <p class="tracking-tighter">
+                내가 필요해서 <br>만든 서비스
+            </p>
+            <p class="text-lg text-indigo-300 select-none mt-10">
+                포트폴리오와 블로그를 결합할 수 있는 서비스가 있으면 좋겠다.<br>비 프로그래머도 사용할 수 있는 직관적인 포트폴리오 사이트를 제작할 수 있으면 좋겠다.<br>
+                디자인은 최대한 심플하고 깔끔하게.<br><br><br>단순 취업용이아닌 진짜 <span class="text-4xl text-indigo-600">" 나 "</span> 에 대한 모든것을 한번에 소개할 수 있는 있었으면 좋겠다.
+            </p>
+        </div>
+        <div class="w-full flex justify-end">
+            <a href="123" class=" rounded-full p-2 px-10 bg-indigo-600 text-white font-bold hover:text-indigo-600 hover:bg-white hover:border-indigo-600 hover:border">시작하기</a>
+        </div>
     </div>
-
-<a href="/" class="flex fixed bottom-12 justify-center w-full">
-    <div class="text-gray-500 text-2xl select-none tracking-[1em]">
-        <p class="border-t-4 p-4 border-indigo-200 hover:border-indigo-600" style="text-indent: 1em;">
-            BLOG
-        </p>
-    </div>
-</a>
-
-<a href="/about" class="flex flex-col fixed top-0 bottom-0 right-24 justify-center h-full"> 
-    <div class="text-gray-500 text-2xl select-none tracking-[1em]">
-        <p class="border-l-4 p-4 border-indigo-200 hover:border-indigo-600" style="writing-mode: vertical-rl; text-orientation: upright; text-indent: 1em;">ABOUT</p>
-    </div>
-</a>
-
-<a href="/" class="flex flex-col fixed top-0 bottom-0 left-24 justify-center h-full"> 
-    <div class="text-gray-500 text-2xl select-none tracking-[1em]">
-        <p class="border-r-4 p-4 border-indigo-200 hover:border-indigo-600" style="writing-mode: vertical-lr; text-orientation: upright; text-indent: 1em;">FOLIO</p>
-    </div>
-</a>
+</div>
---
+++ src/routes/[id]/+page.svelte
@@ -0,0 +1,31 @@
+<script>
+    import { page } from '$app/stores';
+    
+    $: id = $page.params.id;
+</script>
+
+
+<h1 class="font-bold text-indigo-600 select-none text-9xl">CALB</h1>
+    <div class="text-gray-500 text-2xl select-none">
+        Developer
+    </div>
+
+<a href="/{id}/blog" class="flex fixed bottom-12 justify-center w-full">
+    <div class="text-gray-500 text-2xl select-none tracking-[1em]">
+        <p class="border-t-4 p-4 border-indigo-200 hover:border-indigo-600" style="text-indent: 1em;">
+            BLOG
+        </p>
+    </div>
+</a>
+
+<a href="/{id}/about" class="flex flex-col fixed top-0 bottom-0 right-24 justify-center h-full"> 
+    <div class="text-gray-500 text-2xl select-none tracking-[1em]">
+        <p class="border-l-4 p-4 border-indigo-200 hover:border-indigo-600" style="writing-mode: vertical-rl; text-orientation: upright; text-indent: 1em;">ABOUT</p>
+    </div>
+</a>
+
+<a href="/{id}/folio" class="flex flex-col fixed top-0 bottom-0 left-24 justify-center h-full"> 
+    <div class="text-gray-500 text-2xl select-none tracking-[1em]">
+        <p class="border-r-4 p-4 border-indigo-200 hover:border-indigo-600" style="writing-mode: vertical-lr; text-orientation: upright; text-indent: 1em;">FOLIO</p>
+    </div>
+</a>
